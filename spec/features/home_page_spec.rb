cccf100df5895d1e832200a388aacc7d73ddf936
klbris <klbris1029@gmail.com>
2022-05-11 21:37:01 +0000

    Set up deploy for heroku

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -8,6 +8,7 @@ require 'rails_helper'
 RSpec.feature "Visiting the homepage", type: :feature do
   scenario "The visitor should see projects" do
     visit root_path
-    expect(page).to have_text("Thanks for")
+
+    expect(page).to have_text("Thanks")
   end
 end
